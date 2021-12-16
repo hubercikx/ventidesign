ecc4a71e9cdcfed689fc464d73a03d417628fb4b
Hubert <hubcior8@gmail.com>
2021-12-17 00:08:51 +0100

    add script auto year in footer (copyright)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-//scrollTop
-
 const scrollTop = document.querySelector('.scroll-top');
+const footerYear = document.querySelector('.footer-year');
+//scrollTop
 window.addEventListener('scroll', () => {
 	let scroll = document.querySelector('.scroll-top');
 	scroll.classList.toggle('active', window.scrollY > 500);
@@ -11,3 +11,9 @@ scrollTop.addEventListener('click', () => {
 		behavior: 'smooth',
 	});
 });
+//year footer
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
